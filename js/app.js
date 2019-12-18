8beb3a1f160a64be5757330d810fc7eea502e601
raul-octavian <alburaul@gmail.com>
2019-12-18 09:03:10 +0100

    all

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 
-// $(document).ready(function () {
     $('#nav-icon-hamburger').click(function () {
         $(this).toggleClass('open');
         $("#nav-link-one").toggleClass("move-right");
         $("#nav-link-two").toggleClass("move-right");
     });
-// });
 
 
 
@@ -84,7 +82,7 @@ $(".helmet-hover").mouseenter(
 
     });
 
-// ships-genaral-code
+// ships-general-code
 $(".hover-1").mouseenter(
     function () {
         $("#svg-container-parts-hover-1").addClass("visible");
@@ -116,6 +114,41 @@ $(".hover-2").mouseout(
     function () {
         $("#svg-container-parts-hover-2").addClass("not-visible");
         $("#svg-container-parts-hover-2").removeClass("visible");
+        $("#svg-container-parts").removeClass("not-visible");
+        $("#svg-container-parts").addClass("visible");
+
+    });
+
+$(".hover-3").mouseenter(
+    function () {
+        $("#svg-container-parts-hover-3").addClass("visible");
+        $("#svg-container-parts-hover-3").removeClass("not-visible");
+        $("#svg-container-parts").removeClass("visible");
+        $("#svg-container-parts").addClass("not-visible");
+
+    });
+
+$(".hover-3").mouseout(
+    function () {
+        $("#svg-container-parts-hover-3").addClass("not-visible");
+        $("#svg-container-parts-hover-3").removeClass("visible");
+        $("#svg-container-parts").removeClass("not-visible");
+        $("#svg-container-parts").addClass("visible");
+
+    });
+$(".hover-4").mouseenter(
+    function () {
+        $("#svg-container-parts-hover-4").addClass("visible");
+        $("#svg-container-parts-hover-4").removeClass("not-visible");
+        $("#svg-container-parts").removeClass("visible");
+        $("#svg-container-parts").addClass("not-visible");
+
+    });
+
+$(".hover-4").mouseout(
+    function () {
+        $("#svg-container-parts-hover-4").addClass("not-visible");
+        $("#svg-container-parts-hover-4").removeClass("visible");
         $("#svg-container-parts").removeClass("not-visible");
         $("#svg-container-parts").addClass("visible");
 
